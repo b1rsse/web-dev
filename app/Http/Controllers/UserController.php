1db5b83511e09a250cada8c76f6dcb4650668190
Berna Rose <122027178+b1rsse@users.noreply.github.com>
2025-02-27 09:51:26 +0800

    First commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class UserController extends Controller {}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,35 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\StudentsController;
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+
+
+
+// Students Routes
+Route::get('/students', [StudentsController::class, 'index'])->name('students.index');  // Protected
+Route::get('/students/search', [StudentsController::class, 'search'])->name('students.search');  // Protected
+
+// Student Edit Routes
+Route::get('/students/{id}/edit', [StudentsController::class, 'edit'])->name('students.edit');  // Protected
+Route::put('/students/{id}', [StudentsController::class, 'update'])->name('students.update');  // Protected
+
+// Student Delete Routes
+Route::delete('/students/{id}', [StudentsController::class, 'destroy'])->name('students.destroy');  // Protected
+
+// Add new student (protected)
+Route::post('/add-new', [StudentsController::class, 'addNewStudent'])->name('std.addNewStudent');  // Protected
+
+// Public Students View Route (If needed to be public)
+Route::get('/', [StudentsController::class, 'myView'])->name('std.myView'); // Public if required
+
+// Example: Public view for home page (no authentication needed)
+Route::get('/home', function () {
+    return view('home');
 });
