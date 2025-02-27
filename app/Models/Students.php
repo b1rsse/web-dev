5205b30aab56e07a12e3634dbb759f77083bab60
Berna Rose <122027178+b1rsse@users.noreply.github.com>
2025-02-27 09:55:21 +0800

    2nd Activity: Add new user

--- app/Models/Students.php
+++ app/Models/Students.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 //this is my table students
 class Students extends Model
 {
+   //2nd activity: Add new user
    protected $table = 'students';
    protected $fillable = ['name', 'age', 'gender'];
 }
