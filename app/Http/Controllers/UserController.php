5609ecd3bd206f9c2be5c04464fa340231ba4f0c
Rose Alingasa <122027178+b1rsse@users.noreply.github.com>
2025-06-06 14:08:48 +0800

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,4 +6,38 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 
-class UserController extends Controller {}
+class UserController extends Controller
+{
+    //usercontroller
+    public function login(Request $request) {
+        $incomingFields = $request->validate([
+            'loginname' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        if (auth()->attempt(['username' => $incomingFields['loginname'], 'password' =>$incomingFields['loginpassword'] ])) {
+            $request->session()->regenerate();
+            return redirect('/');
+        }
+        return back()->withErrors(['login' => 'Invalid login credentials.']);
+    }
+   
+
+    public function logout() {
+        auth()->logout();
+        return redirect('/login');
+    }
+
+    public function register(Request $request) {
+        $incomingFields = $request->validate([
+            'username' => ['required', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'max:10'],
+        ]);
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+        $user = User::create($incomingFields);
+        auth()->login($user);
+        return redirect('/');
+    }
+}
